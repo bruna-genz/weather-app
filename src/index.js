6085b5f46936fce46069a552929118994140a7a6
Bruna <brunagenz92@gmail.com>
2020-05-05 19:42:36 -0300

    initialize npm, webpack and linters

---
+++ src/index.js
@@ -0,0 +1,3 @@
+import "../src/assets/styles.scss"
+
+console.log("hello")
---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    devServer: {
+        contentBase: './dist'
+        },
+    plugins: [
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: './src/index.html'
+        })
+    ],
+    module: {
+         rules: [
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                'style-loader',
+                'css-loader',
+                'sass-loader',
+                ],
+            },
+        ],
+    },
+};
