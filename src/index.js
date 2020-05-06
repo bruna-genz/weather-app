4e1c544440260842edbe7f2f05445520113260e3
Bruna <brunagenz92@gmail.com>
2020-05-06 19:37:48 -0300

    create functionality to get forecast for the next 5 days

--- src/index.js
+++ src/index.js
@@ -12,29 +12,60 @@ const body = document.querySelector("body")
 
 }*/
 
-const getWeather = async (location) => {
+const getCurrentWeather = async (location) => {
     const result = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${key}&units=metric`, { mode: 'cors'})
-    saveData(await result.json())
+    saveCurrentWeather(await result.json())
 }
 
-const saveData = (weatherData) => {
+const getForecast = async (location) => {
+    const result = await fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${location}&appid=${key}&units=metric`, { mode: 'cors'})
+    saveForecast(await result.json())
+}
+
+const saveCurrentWeather = (weatherData) => {
     console.log(weatherData)
     state.cityName = weatherData.name
-    state.description = weatherData.weather[0].description
-    state.coord = weatherData.coord
-    state.temp = weatherData.main.temp
-    state.feel = weatherData.main.feels_like
-    state.wind = weatherData.wind.speed
+    state.day0 = {  description: weatherData.weather[0].description,
+        temp: weatherData.main.temp,
+        feel: weatherData.main.feels_like,
+        wind: weatherData.wind.speed
+    }
+}
+
+const saveForecast = (weatherData) => {
+    state.forecast = []
+    for (let i = 4, day = 1; i < 40; i+=8, day++) {
+        state.forecast.push({ date: weatherData.list[i].dt_txt,
+                             description: weatherData.list[i].weather[0].description,
+                             temp: weatherData.list[i].main.temp,
+                            })
+    }
 }
 
 const renderWeatherInfo = () => {
     const weatherView = weatherInfoView(state)
     body.insertAdjacentHTML("beforeend", weatherView)
+    renderForecast()
+}
+
+const forecastView = (forecastArray) => {
+    const forecastViewArray = forecastArray.map(current => {
+        return `<div><p>${current.date}</p><p>${current.description}</p><p>${current.temp}</p></div>`
+    })
+    return forecastViewArray
+}
+
+const renderForecast = () => {
+    const forecastContainer = document.querySelector("#next-days")
+    forecastView(state.forecast).forEach( dayView => {
+    forecastContainer.insertAdjacentHTML("beforeend", dayView)
+    })
 }
 
 // Events
 searchButton.addEventListener("click", async () => {
-    await getWeather(locationInput.value)
+    await getCurrentWeather(locationInput.value)
+    await getForecast(locationInput.value)
     renderWeatherInfo()
 })
 
--- src/views/weatherInfoView.js
+++ src/views/weatherInfoView.js
@@ -1,20 +1,25 @@
 export const weatherInfoView = (weatherData) => {
+    console.log(weatherData)
+
     const view = `<div id="weather-info">
         <div id="intro">
             <h2>${weatherData.cityName}</h2>
             <p>Wednesday</p>
-            <p>${weatherData.description}</p>
+            <p>${weatherData.day0.description}</p>
         </div>
         <div id="main">
             <div id="left">
                 <img src="">
-                <h1>${weatherData.temp}</h1>
+                <h1>${weatherData.day0.temp}</h1>
                 <p>°C | °F</p>
             </div>
             <div id="right">
-                <p>Fells like: ${weatherData.feel}°</p>
-                <p>Wind: ${weatherData.wind} km/h</p>
+                <p>Fells like: ${weatherData.day0.feel}°</p>
+                <p>Wind: ${weatherData.day0.wind} km/h</p>
             </div>
+        </div>
+        <div id="next-days">
+
         </div>
     </div>`
     
